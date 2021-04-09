7776a1dac27d1299dcee01f0dac4c68074990891
samcesa45 <samcesa45@gmail.com>
2021-04-09 13:53:49 -0700

    failed to add process.env.PORT now updated

--- server.js
+++ server.js
@@ -82,6 +82,7 @@ app.delete('/api/persons/:id', (req, res) => {
 	res.status(204).end();
 });
 
-const PORT = 4001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
